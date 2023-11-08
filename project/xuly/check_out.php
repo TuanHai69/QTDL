8fbf48f4971319ba6ea6e51eb8cb21a609a15adc
Ly Hoang Nghiem <hoangnghiem121@gmail.com>
2023-11-08 10:46:46 +0700

    nghiem 2.3

--- project/xuly/check_out.php
+++ project/xuly/check_out.php
@@ -1,10 +1,19 @@
 <?php
     
-    $statementc= $conn->prepare("select * from banhang.giohang  where makhachhang = :makhachhang");
+    $statementc= $conn->prepare("SELECT * from banhang.giohang
+      where giohang.makhachhang = :makhachhang");
+    $statementf = $conn->prepare("SELECT khachhang.tenkhachhang, khachhang.diachi,khachhang.sodienthoai,khachhang.email
+     from banhang.khachhang 
+      where khachhang.makhachhang = :makhachhang");
+    
     $statementc->bindParam(':makhachhang',$_SESSION['id'] );
+    $statementf->bindParam(':makhachhang',$_SESSION['id'] );
     $i = 0;
     $statementc->execute();
+    $statementf->execute();
     $subtotal =0;
     $total = 0;
     $results=$statementc->fetchAll(PDO::FETCH_ASSOC);
+    $r1=$statementf->fetch(PDO::FETCH_ASSOC);
+    
 ?>
