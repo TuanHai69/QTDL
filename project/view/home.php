2a610a77b4c24a3241526ae8b12d23cc746b604b
Rudeus2909 <trib2005699@student.ctu.edu.vn>
2023-11-08 12:12:29 +0700

    update_home

--- project/view/home.php
+++ project/view/home.php
@@ -97,8 +97,9 @@
                         }
                     ?>
                     <form action="index.php?act=product_detail" method="post">
-                        <input type="hidden" name="masanpham" value=<?= htmlspecialchars($result['masanpham'])?>>
+                        <input type="hidden" name="masanpham" value=<?=$result['masanpham']?>>
                         <td><button class="btn btn-info" name="product_detail" type="submit">Xem chi tiết</button></td>
+                    </form>
                 </tr>
                 <?php endforeach ?>
             </tbody>
